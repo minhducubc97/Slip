8633947d5dba2ab7c0b3f781e2f8fa680b275c58
duke.nguyen <duke.nguyen@visioncritical.com>
2019-05-16 13:30:01 -0700

    First commit

---
+++ Slip/Controllers/SQSController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Slip.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SQSController : ControllerBase
+    {
+	    private string MySlackQueueUrl;
+	    private string MyGlipQueueUrl;
+	    private IAmazonSQS Sqs;
+
+	    public SQSController()
+	    {
+		    MySlackQueueUrl = "https://sqs.ca-central-1.amazonaws.com/997928571690/Slack";
+		    MyGlipQueueUrl = "https://sqs.ca-central-1.amazonaws.com/997928571690/Glip";
+			Sqs = new AmazonSQSClient(RegionEndpoint.CACentral1);
+		}
+
+	    [Route("createtable")]
+	    public IActionResult CreateDynamoDBTable([FromQuery] string queueName)
+	    {
+			Console.WriteLine("********************************************");
+		    Console.WriteLine("Amazon SQS");
+		    Console.WriteLine("********************************************");
+
+		    var sqsRequest = new CreateQueueRequest
+		    {
+			    QueueName = queueName
+			};
+
+		    var createQueueResponse = Sqs.CreateQueueAsync(sqsRequest).Result;
+
+		    var newQueueUrl = createQueueResponse.QueueUrl;
+
+		    var listQueuesRequest = new ListQueuesRequest();
+		    var listQueuesResponse = Sqs.ListQueuesAsync(listQueuesRequest);
+		    foreach (var queueUrl in listQueuesResponse.Result.QueueUrls)
+		    {
+			    Console.WriteLine($"QueueUrl: {queueUrl}");
+		    }
+			return Ok();
+	    }
+
+	    [Route("slackmessage")]
+	    public IActionResult InsertSlackMessage()
+	    {
+		    var sqsMessageRequest = new SendMessageRequest
+		    {
+			    QueueUrl = MySlackQueueUrl,
+				MessageBody = "Email information"
+		    };
+
+		    Sqs.SendMessageAsync(sqsMessageRequest);
+
+			Console.WriteLine("Finished sending message to our SQS queue.\n");
+
+			return Ok();
+	    }
+
+	    [Route("glipmessage")]
+	    public IActionResult InsertGlipMessage()
+	    {
+			var sqsMessageRequest = new SendMessageRequest
+		    {
+			    QueueUrl = MyGlipQueueUrl,
+			    MessageBody = "Email information"
+		    };
+
+		    Sqs.SendMessageAsync(sqsMessageRequest);
+
+		    Console.WriteLine("Finished sending message to our SQS queue.\n");
+
+		    return Ok();
+		}
+	}
+}
---
+++ Slip/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Slip
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			ReadSQS();
+			CreateWebHostBuilder(args).Build().Run();
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>();
+
+		public static void ReadSQS()
+		{
+			var sqs = new AmazonSQSClient(RegionEndpoint.CACentral1);
+
+			var queueUrl = sqs.GetQueueUrlAsync("EmailQueue").Result.QueueUrl;
+
+			var receiveMessageRequest = new ReceiveMessageRequest
+			{
+				QueueUrl = queueUrl
+			};
+
+			var receiveMessageResponse = sqs.ReceiveMessageAsync(receiveMessageRequest).Result;
+
+			foreach (var message in receiveMessageResponse.Messages)
+			{
+				Console.WriteLine("Message \n");
+				Console.WriteLine($"	MessageId: {message.MessageId} \n");
+				Console.WriteLine($"	ReceiptHandle: {message.ReceiptHandle} \n");
+				Console.WriteLine($"	MSD5Body: {message.MD5OfBody}");
+				Console.WriteLine($"	Body: {message.Body}");
+
+				foreach (var attribute in message.Attributes)
+				{
+					Console.WriteLine("Attribute \n");
+					Console.WriteLine($"	Name: {attribute.Key} \n");
+					Console.WriteLine($"	Value: {attribute.Value} \n");
+				}
+
+				var messageReceptHandle = receiveMessageResponse.Messages.FirstOrDefault()?.ReceiptHandle;
+				
+				var deleteRequest = new DeleteMessageRequest
+				{
+					QueueUrl = queueUrl,
+					ReceiptHandle = messageReceptHandle
+				};
+
+				sqs.DeleteMessageAsync(deleteRequest);
+			}
+		}
+	}
+}
